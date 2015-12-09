e091c89eb097bcfcddbca9da1a0f01625e0fc78d
Yu Zheng(Sam) <sam@tradehero.mobi>
2015-12-09 14:39:49 +0800

    Update the git ignore file.

--- src/main/java/com/linterest/api/DefaultPage.java
+++ src/main/java/com/linterest/api/DefaultPage.java
@@ -1,12 +1,17 @@
 package com.linterest.api;
 
 import com.google.sitebricks.At;
+import com.google.sitebricks.headless.Reply;
+import com.google.sitebricks.headless.Service;
+import com.google.sitebricks.http.Get;
 
 /**
  * @author <a href="mailto:lesliesam@hotmail.com"> Sam Yu </a>
  */
-@At("/")
+@At("/") @Service
 public class DefaultPage {
-    private boolean appear = true;
-    public String getMessage() { return "Hello,"; }
+    @Get
+    public Reply<String> getMessage() {
+        return Reply.with("Hello world, from Linterest.");
+    }
 }
