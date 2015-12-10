21da13379dd94cc7a5f987c8ff2a44e78ee63714
Yu Zheng(Sam) <sam@tradehero.mobi>
2015-12-10 10:15:50 +0800

    Restful API for user add and list.

--- src/main/java/com/linterest/api/UserService.java
+++ src/main/java/com/linterest/api/UserService.java
@@ -1,13 +1,19 @@
 package com.linterest.api;
 
+import com.google.inject.name.Named;
 import com.google.sitebricks.At;
 import com.google.sitebricks.client.transport.Json;
 import com.google.sitebricks.headless.Reply;
+import com.google.sitebricks.headless.Request;
 import com.google.sitebricks.headless.Service;
 import com.google.sitebricks.http.Get;
+import com.google.sitebricks.http.Post;
 import com.linterest.HibernateUtil;
 import com.linterest.dto.UserEntity;
+import org.hibernate.Query;
 import org.hibernate.Session;
+
+import java.util.List;
 
 /**
  * @author <a href="mailto:lesliesam@hotmail.com"> Sam Yu </a>
@@ -15,19 +21,44 @@ import org.hibernate.Session;
 @At("/user") @Service
 public class UserService {
 
-    @Get
-    Reply<UserEntity> getUser() {
-        UserEntity entity = new UserEntity();
-        entity.setPassword("123456");
-        entity.setPhoneNum(1234567890);
-        entity.setUserName("ABC ttt");
+    private static final String QUERY_USER = "from UserEntity";
 
+    @At("/all")
+    @Get
+    public Reply<?> getAllUser(Request request) {
         Session session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
-        session.persist(entity);
-        session.getTransaction().commit();
+        Query query = session.createQuery(QUERY_USER);
+        List<UserEntity> list = query.list();
         session.close();
 
-        return Reply.with(entity).as(Json.class);
+        return Reply.with(list).as(Json.class);
+    }
+
+    @At("/:name")
+    @Get
+    public Reply<?> getUser(Request request, @Named("name") String name) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Query query = session.createQuery(QUERY_USER + " where userName = '" + name + "'");
+        List<UserEntity> list = query.list();
+        session.close();
+
+        return Reply.with(list).as(Json.class);
+    }
+
+    @Post
+    public Reply<?> addUser(Request request) {
+        try {
+            UserEntity user = request.read(UserEntity.class).as(Json.class);
+
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.persist(user);
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            return Reply.saying().error();
+        }
+
+        return Reply.saying().ok();
     }
 }
